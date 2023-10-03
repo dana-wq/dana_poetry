44b9bbd41c0e28c003eeac1028f7367e4d33ac71
dana-wq <144427545+dana-wq@users.noreply.github.com>
2023-10-03 11:50:28 +0200

    Create fizz_buzz_test.py

---
+++ tests/fizz_buzz_test.py
@@ -0,0 +1,6 @@
+assert checkio(15) == "Fizz Buzz"
+assert checkio(6) == "Fizz"
+assert checkio(10) == "Buzz"
+assert checkio(7) == "7"
+
+print("The mission is done! Click 'Check Solution' to earn rewards!")
