d403a66ead34d121f9d866e6c6944f58d5d5f5aa
dana-wq <144427545+dana-wq@users.noreply.github.com>
2023-10-03 11:39:32 +0200

    Create pass6.py

---
+++ my_project/pass6.py
@@ -0,0 +1,16 @@
+def is_acceptable_password(password: str) -> bool:
+    unique_chars = set(re.findall(r'[a-zA-Z0-9]', password))
+    if len(unique_chars)<3:
+        return False
+    elif "password" in password.lower():
+        return False
+    elif len(password) > 9:
+        return True
+    elif str.isnumeric(password):
+        return False
+    elif len(password) > 6:        
+        for i in password:
+            if str.isdigit(i):
+                return True
+            
+    return False
