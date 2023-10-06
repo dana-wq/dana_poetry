a238dc27eda1992a4902e676e4cc12e1efcaef28
dana-wq <144427545+dana-wq@users.noreply.github.com>
2023-10-06 14:28:06 +0200

    Create test_fizz_buzz.py

---
+++ tests/test_fizz_buzz.py
@@ -0,0 +1,7 @@
+from my_project import fizz_buzz
+import pytest
+def test_fizz_buzz():
+   assert checkio(15) == "Fizz Buzz"
+   assert checkio(6) == "Fizz"
+   assert checkio(10) == "Buzz"
+   assert checkio(7) == "7"
