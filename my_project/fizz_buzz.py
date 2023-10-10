0054a400c907fa67648887bb43acbb88319b396b
dana-wq <144427545+dana-wq@users.noreply.github.com>
2023-10-10 12:07:26 +0200

    Update fizz_buzz.py

--- my_project/fizz_buzz.py
+++ my_project/fizz_buzz.py
@@ -2,7 +2,7 @@
 Module to implement the Fizz Buzz game.
 """
 def checkio(num: int) -> str:
-     """
+    """
     Returns 'Fizz' if the input is divisible by 3,
     'Buzz' if divisible by 5, 'Fizz Buzz' if divisible by both,
     and the input number as a string otherwise.
