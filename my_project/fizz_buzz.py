91a9f569a33ec8ad41cf52992af6bb7f145fed82
dana-wq <144427545+dana-wq@users.noreply.github.com>
2023-10-10 12:11:48 +0200

    Update fizz_buzz.py

--- my_project/fizz_buzz.py
+++ my_project/fizz_buzz.py
@@ -9,9 +9,8 @@ def checkio(num: int) -> str:
     """
     if num%3 == 0 and num%5 == 0:
         return "Fizz Buzz"
-    elif num%3 == 0:
+    if num%3 == 0:
         return "Fizz"
-    elif num%5 == 0 :
-        return "Buzz"
-    
+    if num%5 == 0 :
+        return "Buzz" 
     return str(num)
