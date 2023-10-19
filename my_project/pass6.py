3809be4eb583dad57b923f710772d38791c98282
dana-wq <144427545+dana-wq@users.noreply.github.com>
2023-10-19 13:56:42 +0200

    Update pass6.py

--- my_project/pass6.py
+++ my_project/pass6.py
@@ -13,10 +13,16 @@ def is_acceptable_password(password: str) -> bool:
     - If length is greater than 6 and contains at least one digit.
     """
     unique_chars = set(re.findall(r'[a-zA-Z0-9]', password))
-    if len(unique_chars) < 3 or "password" in password.lower():
+    if len(unique_chars)<3:
         return False
-    if len(password) > 9 or str.isnumeric(password):
+    if "password" in password.lower():
+        return False
+    if len(password) > 9:
         return True
-    if len(password) > 6 and any(char.isdigit() for char in password):
-        return True
+    if str.isnumeric(password):
+        return False
+    if len(password) > 6:
+        for i in password:
+            if str.isdigit(i):
+                return True
     return False
