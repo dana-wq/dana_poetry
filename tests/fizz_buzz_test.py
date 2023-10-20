844824503d427f8e71b0830eeed25166efeed2a0
dana-wq <144427545+dana-wq@users.noreply.github.com>
2023-10-20 09:42:53 +0200

    Update fizz_buzz_test.py

--- tests/fizz_buzz_test.py
+++ tests/fizz_buzz_test.py
@@ -1,3 +1,19 @@
+"""
+FizzBuzz Test
+
+This module contains unit tests for the FizzBuzz implementation in the `fizz_buzz` module.
+
+Usage:
+    - Ensure that the `fizz_buzz` module is imported from the `my_project` package.
+    - Run the tests using  Pytest.
+
+Test Cases:
+    - Test case 1: `checkio(15)` should return "Fizz Buzz" since 15 is divisible by both 3 and 5.
+    - Test case 2: `checkio(6)` should return "Fizz" since 6 is divisible by 3.
+    - Test case 3: `checkio(10)` should return "Buzz" since 10 is divisible by 5.
+    - Test case 4: `checkio(7)` should return "7" since 7 is neither divisible by 3 nor 5.
+
+"""
 from my_project import fizz_buzz
 import pytest
 def test_fizz_buzz():
